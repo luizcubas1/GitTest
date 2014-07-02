de625fdc205c17bd3ce84597df7fce4f6ec99db2
Luizcubas <luizcubas@gmail.com>
2014-07-02 09:33:13 -0300

    Bug bobo, resolvido

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ var program = function(){
 	
 	var self = this;
 	this.user = 'Fulano';
-	this.age = 35;
+	this.age = 35;	
 	
-	//this.name = 'Fulano de Souza';
 	this.name = '';
 	this.site = 'http://www.fulanosites.com';
 	
 	this.constructor = function(name){
 		self.name = self.setName(name);
-		selfage = self.setAge(newAge);
+		self.age = self.setAge(newAge);
 	}
 	
 	this.setName = function(newname){
